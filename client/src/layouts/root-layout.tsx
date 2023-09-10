ed3b493c8b8f78e1282eba717d4084af75d409e8
imran-h-ob <imran.h.ovi.61@gmail.com>
2023-09-10 22:16:26 +0600

    dashboard basic layout done

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1>Hello well up high school!</h1>
       <Button>Click Me!</Button>
+      <br />
+      <Link href="/dashboard/side-nav">
+        <Button className="my-5">Go To Dashboard</Button>
+      </Link>
     </div>
   );
 };
