57381b6d7947d0aca9678eabdb87ae95884316a6
niaz abir <niazabir55@gmail.com>
2023-09-10 15:30:13 +0600

    navbar commit

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Navbar from "@/components/navbar";
+import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <header className="container">header</header>
+      {/* <header className="container">header</header> */}
+      <Navbar></Navbar>
       <main className="container">{children}</main>
       <footer className="container">footer</footer>
     </>
