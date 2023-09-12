91f429c75cb42a15fd577d9e5afb4ec5ee721cb4
Ador Zamaan <adorzamaan@gmail.com>
2023-09-13 00:02:52 +0600

    Implement Add student addition feature

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Button } from '@/components/ui/button';
+import AddStudent from "@/components/AddStudent/AddStudent";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
       <h1>Hello well up high school!</h1>
       <Button>Click Me!</Button>
+      <AddStudent />
     </div>
   );
 };
