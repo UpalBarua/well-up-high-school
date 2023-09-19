3677a09cfed4ad4f018acc70fa087dcd12c6773a
Upal Barua <upalbarua52@gmail.com>
2023-09-19 12:42:14 +0600

    made some changes

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       {children}
     </>
   );
