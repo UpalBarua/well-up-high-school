e16f1526d2e3ae75b104c535e628363960d48f15
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-09-19 23:11:24 +0600

    teacher card added

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,13 +1,17 @@
 import Navbar from '@/components/navbar';
+import TeachersList from '@/pages/teachers';
 import React from 'react';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <main className="container">{children}</main>
+      <main className="container">
+        {children}
+        </main>
     </>
   );
 };
+
 
 export default RootLayout;
