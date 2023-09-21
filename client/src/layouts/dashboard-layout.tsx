3e06b2417212bf517ab4baa5fa2a6376a81ef72e
Upal Barua <upalbarua52@gmail.com>
2023-09-21 10:39:28 +0600

    Added fixed sidebar in dashboard-layout

--- client/src/layouts/dashboard-layout.tsx
+++ client/src/layouts/dashboard-layout.tsx
@@ -2,10 +2,23 @@ import { ReactNode } from 'react';
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <aside>sidebar with navigation!</aside>
-      {children}
-    </>
+    <div className="container flex gap-5 relative">
+      <aside className="bg-red-200 p-5 w-56 hidden md:block">
+        <div className="fixed h-full">
+          <h2>Main Menu</h2>
+          <ul>
+            <li>Overview</li>
+            <li>Notices</li>
+            <li>Clubs</li>
+            <li>Gallery</li>
+            <li>Teachers</li>
+            <li>Students</li>
+            <li>Management</li>
+          </ul>
+        </div>
+      </aside>
+      <main className="flex-1">{children}</main>
+    </div>
   );
 };
 
